00947d7c2cb3716a271beea469b5d44b32c21b44
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2020-12-22 01:12:58 +0300

    fix: автотесты ревью 1

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,23 +1,30 @@
 let editFormPopup = document.querySelector('.popup');
 let editFormCloseButton = document.querySelector('.popup__close');
 let profileEditButton = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.popup__body')
+let formElement = document.querySelector('.popup__container')
 
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_subline');
+
+let nameField = document.querySelector('.profile__name');
+let avatar = document.querySelector('.profile__avatar');
+let sublineField = document.querySelector('.profile__subline');
+
+function popupOpen() { 
+    nameInput.value = nameField.textContent;
+    jobInput.value = sublineField.textContent;
+    editFormPopup.classList.add('popup_opened');
+}
+    
 function popupClose() { 
     editFormPopup.classList.remove('popup_opened');
 }
 
-profileEditButton.addEventListener('click', function() { editFormPopup.classList.add('popup_opened') } );
+profileEditButton.addEventListener('click', popupOpen);
 editFormCloseButton.addEventListener('click', popupClose);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('input[name="name"]');
-    let jobInput = document.querySelector('input[name="subline"]');
-
-    let nameField = document.querySelector('.profile__name');
-    let avatar = document.querySelector('.profile__avatar');
-    let sublineField = document.querySelector('.profile__subline');
 
     nameField.textContent = nameInput.value;
     avatar.alt = nameInput.value;
