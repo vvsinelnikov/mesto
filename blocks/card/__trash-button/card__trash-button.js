06b6fed5bc3941936eec8027122135519d914ac5
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-01-21 02:50:10 +0300

    feat: поправить js

--- blocks/card/__trash-button/card__trash-button.js
+++
@@ -1,8 +0,0 @@
-/*
-этот код нужен только для карточек, прописанных в html
-
-const trashButton = document.querySelectorAll('.card__trash-button');
-trashButton.forEach(function(cardNumber) {
-    cardNumber.addEventListener('click', function() {this.closest('li').remove();})
-});
-*/
